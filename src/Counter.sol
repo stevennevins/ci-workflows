6c3d1619602d2ec408c6992f91bb9e666e135dd9
steven nevins <steven.nevins.js@gmail.com>
2024-05-16 07:39:25 -0400

    add docs workflow

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,13 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+/**
+ *@title A simple counter contract
+ * @dev This contract allows you to increment and set a number stored on the blockchain.
+ * The contract is designed to demonstrate basic Solidity concepts and state management.
+ */
 contract Counter {
+    /// @notice The current value of the counter
+    /// @dev Stored as a uint256, this variable holds the state of the counter's value.
     uint256 public number;
 
+    /**
+     * @notice Sets the counter to a new value
+     * @dev Assigns a new value to the `number` state variable.
+     * @param newNumber The new value to set `number` to.
+     */
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
 
+    /**
+     * @notice Increments the counter by one
+     * @dev Adds one to the current value of `number`.
+     * This function demonstrates how to modify state variables.
+     */
     function increment() public {
         number++;
     }
