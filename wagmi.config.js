823fdea3648f7d472d5b6ff717ac1e8e25df339b
steven nevins <steven.nevins.js@gmail.com>
2024-05-17 07:57:51 -0400

    build: update wagmi config

--- wagmi.config.js
+++ wagmi.config.js
@@ -1,14 +1,17 @@
-import { foundry } from '@wagmi/cli/plugins'
+import { foundry, actions } from "@wagmi/cli/plugins";
 
 // @ts-check
 /** @type {import('@wagmi/cli').Config} */
 export default {
-  out: 'dist/generated.js',
-  plugins: [foundry({
-    forge: {
-      clean: false,
-      build: false,
-      rebuild: false
-    }
-  })],
-}
+	out: "dist/generated.js",
+	plugins: [
+		actions(),
+		foundry({
+			forge: {
+				clean: false,
+				build: false,
+				rebuild: false,
+			},
+		}),
+	],
+};
