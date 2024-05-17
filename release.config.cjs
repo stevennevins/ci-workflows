308e199f19db463dd72c5d4accfabfc674068686
steven nevins <steven.nevins.js@gmail.com>
2024-05-17 10:32:51 -0400

    feat: update release rules

--- release.config.cjs
+++ release.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      releaseRules: [
+        { type: 'feat', release: 'minor' },
+        { breaking: true, release: 'major' },
+        { release: 'patch' }
+      ],
+    }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/github'
